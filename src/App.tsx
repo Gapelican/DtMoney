460ba04c9a776ebc91c38a347689edabf066b6b3
Gabriel Pelicano <gapelicano97@gmail.com>
2021-09-09 20:49:10 -0300

    Project in progress

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Dashbord } from "./components/Dashbord";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Dashbord />
+      <GlobalStyle />
+    </>
+  );
+}
+
+
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
+import { App } from './App';
+
+<link rel="shortcut icon" href="favicon.png" type="image/png" />
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
